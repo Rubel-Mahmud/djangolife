ee3bc18816ef40a579456a07a82ca7fdca96f4a8
Rubel Mahmud <rmsoft.com@gmail.com>
2020-11-23 06:57:46 -0800

    create login view and template, add some style

--- djangolife/urls.py
+++ djangolife/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from video.views import index
+from video.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
     path('', index, name='index'),
+    path('login/', SignIn, name='signin'),
 ]
--- video/views.py
+++ video/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from django.contrib.auth import authenticate
 
 from video.models import video
 
@@ -6,4 +8,19 @@ def index(request):
     songs = video.objects.all().order_by('-id')
     return render(request, 'video/index.html', {
         'songs':songs,
-    });
+    })
+
+def SignIn(request):
+    if request.method == 'POST':
+        username = request.POST.get('username', None)
+        password = request.POST.get('password', None)
+
+        if username is not None and password is not None :
+            user = authenticate(username=username, password=password)
+            if user:
+                return redirect('/')
+            else:
+                return HttpResponse('login failed')
+        else:
+            return HttpResponse('Empty value entered.')
+    return render(request, 'video/login.html')
